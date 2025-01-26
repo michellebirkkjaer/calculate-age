f62b69980c47bbf1cd85cdce1d056391254cbc94
michellebirkkjaer <michelle.birkkjaer@hotmail.dk>
2025-01-26 16:21:47 +0100

    Opgave løst

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
 "use strict";
+
+
+const birthYear = prompt("Indtast dit fødselsår:");
+
+const currentYear = new Date().getFullYear();
+
+const age = currentYear - parseInt(birthYear);
+
+console.log(`Du er ${age} år gammel.`);
